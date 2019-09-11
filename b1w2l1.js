85c4f0ddc2797178e3901d136c557bc6b7f8f543
Rowin Bodegom <99056991@mydavinci.nl>
2019-09-11 11:36:33 +0200

    Opdracht volbracht

---
+++ b1w2l1.js
@@ -0,0 +1,5 @@
+var firstname = prompt("Voer je voornaam in");
+var secondname = prompt("Voer je achternaam in");
+var age = prompt("Voer je leeftijd in");
+alert(firstname + secondname + age);
+document.write(firstname + " " + secondname + "<p> Da Vinci");
